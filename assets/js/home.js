e0c85a3d5f0e2d9c2f3b83c1ee2058717602e700
Kevin Simon <keviniansimon@gmail.com>
2020-11-20 21:48:35 +0200

    Makrup selected projects section

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,8 @@
 particlesJS.load('particles-js', 'assets/configs/particles.json');
+
+var macyInstance = Macy({
+    container: ".macy-container",
+    columns: 2,
+    trueOrder: true,
+    margin: 80
+  });
