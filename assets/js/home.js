e016d834b9ff14d86fa6c81b5b3166e4c720b7f6
Kevin Simon <keviniansimon@gmail.com>
2020-11-21 23:24:12 +0200

    Style featured projects and footer

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,50 @@
 particlesJS.load('particles-js', 'assets/configs/particles.json');
 
-var macyInstance = Macy({
-    container: ".macy-container",
-    columns: 2,
-    trueOrder: true,
-    margin: 80
+document.querySelectorAll('#featured-project').forEach((project) => {
+  project.addEventListener('mouseenter', (e) => {
+    if (e.target.classList.contains('huddle')) {
+      document.body.classList.toggle('huddle');
+    } else if (e.target.classList.contains('axio')) {
+      document.body.classList.toggle('axio');
+    } else if (e.target.classList.contains('kevin')) {
+      document.body.classList.toggle('kevin');
+      const randomNumber = Math.floor(Math.random() * 6);
+      const bodyKevin = document.querySelector('.kevin');
+
+      switch (randomNumber) {
+        case 0:
+          bodyKevin.style.backgroundColor = '#2C4AFF';
+          break;
+        case 1:
+          bodyKevin.style.backgroundColor = '#3DE4A3';
+          break;
+        case 2:
+          bodyKevin.style.backgroundColor = '#FFB739';
+          break;
+        case 3:
+          bodyKevin.style.backgroundColor = '#FC4FB1';
+          break;
+        case 4:
+          bodyKevin.style.backgroundColor = '#BB38FF';
+          break;
+        case 5:
+          bodyKevin.style.backgroundColor = '#FC4F4F';
+          break;  
+      }
+    }
   });
+});
+
+document.querySelectorAll('#featured-project').forEach((project) => {
+  project.addEventListener('mouseleave', (e) => {
+    if (e.target.classList.contains('huddle')) {
+      document.body.classList.toggle('huddle');
+    } else if (e.target.classList.contains('axio')) {
+      document.body.classList.toggle('axio');
+    } else if (e.target.classList.contains('kevin')) {
+      const bodyKevin = document.querySelector('.kevin');
+      bodyKevin.style.backgroundColor = null;
+      document.body.classList.toggle('kevin');
+    }
+  });
+});
--- assets/js/lightdark.js
+++ assets/js/lightdark.js
@@ -1,39 +1,42 @@
-const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+const userPrefersDark =
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches;
+const userPrefersLight =
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: light)').matches;
 
 document.querySelector('.toggle-button').addEventListener('click', () => {
-    let lightMode,
-        darkMode;
+  let lightMode, darkMode;
 
-    if(localStorage.getItem('lightMode') === null) {
-        lightMode = [];
-    } else {
-        localStorage.removeItem('lightMode');     
-    }
+  if (localStorage.getItem('lightMode') === null) {
+    lightMode = [];
+  } else {
+    localStorage.removeItem('lightMode');
+  }
 
-    if(localStorage.getItem('darkMode') === null) {
-        darkMode = [];
-    } else {
-        localStorage.removeItem('darkMode');
-    }
+  if (localStorage.getItem('darkMode') === null) {
+    darkMode = [];
+  } else {
+    localStorage.removeItem('darkMode');
+  }
 
-    document.body.classList.toggle('dark');
+  document.body.classList.toggle('dark');
 
-    if(document.body.classList.contains('dark')) {
-        darkMode.push('dark');
-        localStorage.setItem('darkMode', darkMode);
-    } else {
-        lightMode.push('light');
-        localStorage.setItem('lightMode', lightMode);
-    }
-})
+  if (document.body.classList.contains('dark')) {
+    darkMode.push('dark');
+    localStorage.setItem('darkMode', darkMode);
+  } else {
+    lightMode.push('light');
+    localStorage.setItem('lightMode', lightMode);
+  }
+});
 
-if(userPrefersDark){
-    document.body.classList.add('dark');
+if (userPrefersDark) {
+  document.body.classList.add('dark');
 }
 
-if(localStorage.getItem('darkMode') === 'dark') {
-    document.body.classList.add('dark');
+if (localStorage.getItem('darkMode') === 'dark') {
+  document.body.classList.add('dark');
 } else if (localStorage.getItem('lightMode') === 'light') {
-    document.body.classList.remove('dark');
+  document.body.classList.remove('dark');
 }
